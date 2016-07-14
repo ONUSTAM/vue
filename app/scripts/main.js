8979de4a6aa96a7111be8869522f3b6e1933c8a9
matsuno-t <matsuno-t@iij.ad.jp>
2016-07-14 12:10:52 +0900

    upadte

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -52,6 +52,16 @@ function abc () {
     console.log('abc');
 }
 
-// function doSomething () {
-//   console.log('doSomething');
-// }
+
+// 定義する
+var MyComponent = Vue.extend({
+  template: '<div>A custom component!</div>'
+})
+
+// 登録する
+Vue.component('my-component', MyComponent)
+
+// root インスタンスを作成する
+new Vue({
+  el: '#example'
+})
